d69051e9e18a0937d506508f06bd3fa8c6fba8f1
crazydrace <ajua46244@gmail.com>
2025-06-29 12:03:42 +0530

    Add root route to confirm API is running

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running 💨");
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
